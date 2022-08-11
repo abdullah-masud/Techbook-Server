15132a525cdaebbb6e0222e048f4f848a7149501
Abdullah Al Masud <masudctg00@gmail.com>
2022-08-11 23:46:35 +0600

    load specific blog api written

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aplzjwc.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const allBlogsCollection = client.db('TechBook').collection('allblogs');
+
+        // GET all blogs from DB
+        app.get('/allblogs', async (req, res) => {
+            const query = {};
+            const cursor = allBlogsCollection.find(query);
+            const allblogs = await cursor.toArray();
+            res.send(allblogs);
+        })
+
+        // GET one part using specific ID
+        app.get('/allblogs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const blog = await allBlogsCollection.findOne(query);
+            res.send(blog);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello From TechBook')
