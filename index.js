dfdea20a74ac2a606b891e543d0ffccb1b346bfa
Abdullah Al Masud <masudctg00@gmail.com>
2022-08-24 03:52:29 +0600

    all done

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ async function run() {
             res.send(categorisedBlogs)
         })
 
-        // PUT user in DB
+        // PUT user and update profile pic in DB
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -81,21 +81,6 @@ async function run() {
             res.send(result);
         })
 
-        // PUT user image in DB (update)
-        app.put('/user/:email', async (req, res) => {
-            const email = req.params.email;
-            const user = req.body;
-            const filter = { email: email };
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-
-                }
-            }
-            const result = await usersCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-        })
-
         // GET user from db
         app.get('/users', async (req, res) => {
             const query = {};
@@ -104,7 +89,7 @@ async function run() {
             res.send(allUsers);
         })
 
-        // GET user from db based on email
+        // GET user from db based on email for My Profile
         app.get('/userprofile', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
